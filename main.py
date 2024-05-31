ecdc65591718f83bc9066975729c0fbd14f3a2ec
Santietec <s.marthi@alumno.etec.um.edu.ar>
2024-05-31 12:35:55 -0300

    saludo

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def index():
 def goodbye():
     return "chau"
 
-@app.route("/saludo")
-def salute():
-    return "Hola[santi]"
+@app.route("/saludo/<nombre>")
+def salute(nombre):
+    return f"Hola, {nombre}"
 
 app.run()
